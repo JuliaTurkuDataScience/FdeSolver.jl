69668c1cb530a1f2f98322a2ac4d2c8b22fe8f4d
Giulio Benedetti <giuliobene2000@gmail.com>
2021-09-24 00:27:52 +0300

    Improve main

--- src/main.jl
+++ src/main.jl
@@ -1,8 +1,4 @@
-"""
-    FDEsolver(F, tSpan, y0, β, par...; h = 0.01, nc = 2, tol = 10^(-6), itmax = 30)
-Solves fractional differential equations with a predictor-corrector approach.
-"""
-function FDEsolver(F, tSpan, y0, β, par...; h = 0.01, nc = 2, tol = 10^(-6), itmax = 30)
+function FDEsolver(F, tSpan, y0, β, par...; h = 0.01, nc::Any = 3, tol = 10e-10, itmax = 10)
 
     # Time discretization
     N::Int64 = cld(tSpan[2] - tSpan[1], h)
@@ -23,15 +19,32 @@ function FDEsolver(F, tSpan, y0, β, par...; h = 0.01, nc = 2, tol = 10^(-6), it
             # Y1
             Y[2, :] .= T0 .+ h .^ β .* F(t, n, β, Y, par...) ./ Γ(β .+ 1)
 
-            for j in 1:nc
+            if nc == "Convergence"
 
-                # Y11
-                Y11 = T0 .+ h .^ β .* β .* F(t, n, β, Y, par...) ./ Γ(β .+ 2) .+ h .^ β .* F(t, n + 1, β, Y, par...) ./ Γ(β .+ 2)
-                σ = sqrt(sum((Y11 .- Y[2, :]) .^ 2))
-                Y[2, :] .= Y11
+                σ = 1.1 * tol
 
-                if (σ < tol || j >= itmax)
-                    break
+                while (σ > tol || j < itmax)
+
+                    # Y11
+                    Y11 = T0 .+ h .^ β .* β .* F(t, n, β, Y, par...) ./ Γ(β .+ 2) .+ h .^ β .* F(t, n + 1, β, Y, par...) ./ Γ(β .+ 2)
+                    σ = sqrt(sum((Y11 .- Y[2, :]) .^ 2))
+                    Y[2, :] .= Y11
+
+                end
+
+            else
+
+                for j in 1:nc
+
+                    # Y11
+                    Y11 = T0 .+ h .^ β .* β .* F(t, n, β, Y, par...) ./ Γ(β .+ 2) .+ h .^ β .* F(t, n + 1, β, Y, par...) ./ Γ(β .+ 2)
+                    σ = sqrt(sum((Y11 .- Y[2, :]) .^ 2))
+                    Y[2, :] .= Y11
+
+                    if σ < tol
+                        warn('Correction below tolerance!')
+                    end
+
                 end
 
             end
@@ -43,15 +56,32 @@ function FDEsolver(F, tSpan, y0, β, par...; h = 0.01, nc = 2, tol = 10^(-6), it
             # Yp
             Y[n + 1, :] .= T0 .+ h .^ β .* (ϕ .- α(0, β) .* F(t, n - 1, β, Y, par...) .+ 2 .* α(0, β) .* F(t, n, β, Y, par...))
 
-            for j in 1:nc
+            if nc == "Convergence"
 
-                # Y2
-                Y2 = T0 .+ h .^ β .* (ϕ .+ α(0, β) .* F(t, n + 1, β, Y, par...))
-                σ = sqrt(sum((Y2 .- Y[2, :]) .^ 2))
-                Y[n + 1, :] .= Y2
+                σ = 1.1 * tol
 
-                if (σ < tol || j >= itmax)
-                    break
+                while (σ > tol || j < itmax)
+
+                    # Y2
+                    Y2 = T0 .+ h .^ β .* (ϕ .+ α(0, β) .* F(t, n + 1, β, Y, par...))
+                    σ = sqrt(sum((Y2 .- Y[2, :]) .^ 2))
+                    Y[n + 1, :] .= Y2
+
+                end
+
+            else
+
+                for j in 1:nc
+
+                    # Y2
+                    Y2 = T0 .+ h .^ β .* (ϕ .+ α(0, β) .* F(t, n + 1, β, Y, par...))
+                    σ = sqrt(sum((Y2 .- Y[2, :]) .^ 2))
+                    Y[n + 1, :] .= Y2
+
+                    if σ < tol
+                        warn('Correction below tolerance!')
+                    end
+
                 end
 
             end
