1028487239b16216eddf59924b0306a2e9df6aa5
RiboRings <82203661+RiboRings@users.noreply.github.com>
2021-09-28 16:55:35 +0300

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -5,19 +5,20 @@ function FDEsolver(F, tSpan, y0, β, ::Nothing, par...; h = 0.01, nc = 3, StopIt
     t = tSpan[1] .+ collect(0:N) .* h
 
     # Enter the initial values
-    Y = defineY(N, y0)
+    Y = defineY(N, y0, β)
 
     # Calculate T with taylor expansion
     m::Int64 = ceil(β[1])
 
     for n in 1:N
 
-        T0 = taylor_expansion(tSpan[1], t[n], y0)
+        T0 = taylor_expansion(tSpan[1], t[n], y0, β, m)
 
         if n == 1
 
             # Y1
-            Y[2, :] .= T0 .+ h .^ β .* F(t, n, β, Y, par...) ./ Γ(β .+ 1)
+            Y1 = T0 .+ h .^ β .* F(t, n, β, Y, par...) ./ Γ(β .+ 1)
+            Y = indexY(n, Y, Y1)
 
             if StopIt == "Standard"
 
@@ -25,8 +26,8 @@ function FDEsolver(F, tSpan, y0, β, ::Nothing, par...; h = 0.01, nc = 3, StopIt
 
                     # Y11
                     Y11 = T0 .+ h .^ β .* β .* F(t, n, β, Y, par...) ./ Γ(β .+ 2) .+ h .^ β .* F(t, n + 1, β, Y, par...) ./ Γ(β .+ 2)
-                    σ = sqrt(sum((Y11 .- Y[2, :]) .^ 2))
-                    Y[2, :] .= Y11
+                    σ = sqrt(sum((Y11 .- Y[n + 1, :]) .^ 2))
+                    Y = indexY(n, Y, Y11)
 
                 end
 
@@ -39,8 +40,8 @@ function FDEsolver(F, tSpan, y0, β, ::Nothing, par...; h = 0.01, nc = 3, StopIt
 
                     # Y11
                     Y11 = T0 .+ h .^ β .* β .* F(t, n, β, Y, par...) ./ Γ(β .+ 2) .+ h .^ β .* F(t, n + 1, β, Y, par...) ./ Γ(β .+ 2)
-                    σ = sqrt(sum((Y11 .- Y[2, :]) .^ 2))
-                    Y[2, :] .= Y11
+                    σ = sqrt(sum((Y11 .- Y[n + 1, :]) .^ 2))
+                    Y = indexY(n, Y, Y11)
 
                     j += 1
 
@@ -53,7 +54,8 @@ function FDEsolver(F, tSpan, y0, β, ::Nothing, par...; h = 0.01, nc = 3, StopIt
             ϕ = Phi(Y, F, β, t, n, par...)
 
             # Yp
-            Y[n + 1, :] .= T0 .+ h .^ β .* (ϕ .- α(0, β) .* F(t, n - 1, β, Y, par...) .+ 2 .* α(0, β) .* F(t, n, β, Y, par...))
+            Yp = T0 .+ h .^ β .* (ϕ .- α(0, β) .* F(t, n - 1, β, Y, par...) .+ 2 .* α(0, β) .* F(t, n, β, Y, par...))
+            Y = indexY(n, Y, Yp)
 
             if StopIt == "Standard"
 
@@ -62,7 +64,7 @@ function FDEsolver(F, tSpan, y0, β, ::Nothing, par...; h = 0.01, nc = 3, StopIt
                     # Y2
                     Y2 = T0 .+ h .^ β .* (ϕ .+ α(0, β) .* F(t, n + 1, β, Y, par...))
                     σ = sqrt(sum((Y2 .- Y[n + 1, :]) .^ 2))
-                    Y[n + 1, :] .= Y2
+                    Y = indexY(n, Y, Y2)
 
                 end
 
@@ -76,7 +78,7 @@ function FDEsolver(F, tSpan, y0, β, ::Nothing, par...; h = 0.01, nc = 3, StopIt
                     # Y2
                     Y2 = T0 .+ h .^ β .* (ϕ .+ α(0, β) .* F(t, n + 1, β, Y, par...))
                     σ = sqrt(sum((Y2 .- Y[n + 1, :]) .^ 2))
-                    Y[n + 1, :] .= Y2
+                    Y = indexY(n, Y, Y2)
 
                     j += 1
 
