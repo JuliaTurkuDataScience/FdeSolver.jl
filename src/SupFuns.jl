35e2ee62c0ab3fa85cf7b0fc0968200d4e45273f
MoeinKhalighi <moein.khalighi@gmail.com>
2021-09-29 12:03:36 +0300

    An example with order=2 has been  added in folder Examples and test.

--- src/SupFuns.jl
+++ src/SupFuns.jl
@@ -26,13 +26,13 @@ function defineY(N, y0::Vector{<:Real}, β)
 
 end
 
-function defineY(N, y0::Matrix{<:Real}, β)
-
-    Y = zeros(N + 1, size(y0)[2])
-    Y[1, :] .= y0[1, :]
-    Y
-
-end
+# function defineY(N, y0::Matrix{<:Real}, β)
+#
+#     Y = zeros(N + 1, size(y0)[2])
+#     Y[1, :] .= y0[1, :]
+#     Y
+#
+# end
 
 ## indexY ##
 function indexY(n, Y::Vector{<:Real}, Ynext)
@@ -65,8 +65,8 @@ function taylor_expansion(t0, t, y0::Vector{<:Real}, β, m)
     end
 
 end
-
-taylor_expansion(t0, t, y0::Matrix{<:Real}, β, m) = sum([(t - t0) ^ k / factorial(k) * y0[:, k + 1] for k in 0:m - 1])
+#
+# taylor_expansion(t0, t, y0::Matrix{<:Real}, β, m) = sum([(t - t0) ^ k / factorial(k) * y0[:, k + 1] for k in 0:m - 1])
 
 ## function ϕ ##
 function Phi(y, F, β, t, n, par...)
@@ -80,26 +80,6 @@ function Phi(y, F, β, t, n, par...)
     a_n0(n, β) .* F(t, 1, β, y, par...) .+ sum([alpha[i] .* fun[i] for i in 1:n - 1])
 
 end
-
-a_n0(n, β) = ((n - 1) .^ (β .+ 1) .- n .^ β .* (n .- β .- 1)) ./ Γ(β .+ 2)
-
-## function α ##
-function α(n, β)
-
-    if n == 0
-
-        return 1 ./ Γ(β .+ 2)
-
-    else
-
-        return ((n .- 1) .^ (β .+ 1) .- 2 .* n .^ (β .+ 1) .+ (n .+ 1) .^ (β .+ 1)) ./ Γ(β .+ 2)
-
-    end
-
-end
-
-## Gamma function for vectors ##
-Γ(b) = map(gamma, b)
 
 a_n0(n, β) = ((n - 1) .^ (β .+ 1) .- n .^ β .* (n .- β .- 1)) ./ Γ(β .+ 2)
 
